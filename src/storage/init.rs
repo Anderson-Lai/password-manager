ae2d139a5a7bac6d1c05413f7e84c56f1679e7cf
Anderson-Lai <andersonmhlai26@gmail.com>
2025-04-29 13:10:11 -0400

    add initial password needed for other commands

--- src/storage/init.rs
+++ src/storage/init.rs
@@ -1,5 +1,5 @@
 use std::{collections::HashMap, fs::OpenOptions, io::Write, path::Path};
-use crate::{constants, encryption::encrypt::EncryptedPassword};
+use crate::{commands::{get_master::get_master_password, handle_create::handle_random_create}, constants, encryption::encrypt::EncryptedPassword};
 use super::read::read_saved_passwords;
 
 pub fn init_data() -> Result<HashMap<String, EncryptedPassword>, ()> {
@@ -25,11 +25,28 @@ pub fn init_data() -> Result<HashMap<String, EncryptedPassword>, ()> {
     };
 
     let contents = read_saved_passwords();
-    match contents {
-        Ok(v) => Ok(v),
+    let mut contents = match contents {
+        Ok(v) => v,
         Err(_) => {
             eprintln!("Error while reading saved passwords from {}", constants::FILE_NAME);
-            Err(())
+            return Err(());
         }
+    };
+
+    // an already hashed password under the application name 'password-manager' is
+    // needed for some commands
+    if !contents.contains_key("password-manager") {
+        println!("Please enter your master password to initialize the password manager.");
+        let master_password = get_master_password();
+        match handle_random_create(&mut contents, "password-manager", &master_password, 20, true, false) {
+            Ok(_) => Ok(contents),
+            Err(_) => {
+                eprintln!("Error during intializing password creation!");
+                Err(())
+            }
+        }
+    }
+    else {
+        Ok(contents)
     }
 }
