2f09d074311b441983c04e60669f84200a55d13f
Anderson-Lai <andersonmhlai26@gmail.com>
2025-04-29 20:08:41 -0400

    add more comments to password generation

--- src/password/create.rs
+++ src/password/create.rs
@@ -10,10 +10,13 @@ pub fn create_random_password(length: usize, include_special_characters: bool) -
                 let index = rng.gen_range(0..constants::PASSWORD_CHARACTERS.len());
                 constants::PASSWORD_CHARACTERS[index] as char
             })
+            // collects each iteration of the map which returns a char into a String
             .collect()
     }
     else {
+        // create a random infinite iterator over Alphanumeric
         rng.sample_iter(&Alphanumeric)
+            // take the first 'length' characters
             .take(length)
             // char::from is a static function converting u8 to char
             .map(char::from)
